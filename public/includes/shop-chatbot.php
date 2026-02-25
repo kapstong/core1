60d98f32138f33d33c6ea72260ed71b711031e71
Jerex20 <jerexrb@gmail.com>
2026-02-25 21:12:04 +0800

    Add cache-busting for chatbot assets

--- public/includes/shop-chatbot.php
+++ public/includes/shop-chatbot.php
@@ -3,6 +3,11 @@ if (defined('PPC_SHOP_CHATBOT_INCLUDED')) {
     return;
 }
 define('PPC_SHOP_CHATBOT_INCLUDED', true);
+
+$chatbotCssPath = __DIR__ . '/../assets/css/shop-chatbot.css';
+$chatbotJsPath = __DIR__ . '/../assets/js/shop-chatbot.js';
+$chatbotCssVersion = file_exists($chatbotCssPath) ? (string)filemtime($chatbotCssPath) : '1';
+$chatbotJsVersion = file_exists($chatbotJsPath) ? (string)filemtime($chatbotJsPath) : '1';
 ?>
-<link rel="stylesheet" href="assets/css/shop-chatbot.css">
-<script src="assets/js/shop-chatbot.js" defer></script>
+<link rel="stylesheet" href="assets/css/shop-chatbot.css?v=<?php echo rawurlencode($chatbotCssVersion); ?>">
+<script src="assets/js/shop-chatbot.js?v=<?php echo rawurlencode($chatbotJsVersion); ?>" defer></script>
